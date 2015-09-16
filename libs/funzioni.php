edb5f648c10b3d8ff8fdd9121ab9eddeac1df226
siriguismaele <sirigu.ismaele@gmail.com>
2015-09-16 19:36:54 +0200

    Ultima commit

--- libs/funzioni.php
+++ libs/funzioni.php
@@ -1,7 +1,7 @@
 <?php	
 	define("DB_SERVER", "localhost"); 
-	define("DB_USER", "root"); 
-	define("DB_PASS", "davide"); 
+	define("DB_USER", "siriguIsmaele"); 
+	define("DB_PASS", "armadillo3652"); 
 	define("DB_NAME", "amm15_siriguIsmaele");
 
 	function aggiungiProdotto($nome, $costo, $quantita){
